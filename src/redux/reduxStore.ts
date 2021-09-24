46122403a449a873f35d19295fb4d9a9719d5fd2
IngvarrFX <64922169+IngvarrFX@users.noreply.github.com>
2021-09-24 16:28:16 +0300

    refactor

--- src/redux/reduxStore.ts
+++ src/redux/reduxStore.ts
@@ -1,8 +1,7 @@
-import {combineReducers, createStore} from 'redux';
-import {dialogsReducer} from './dialogsReducer';
-import {messageReducer} from './messageReducer';
+import {combineReducers, createStore} from "redux";
+import {dialogsReducer} from "./dialogsReducer";
+import {messageReducer} from "./messageReducer";
 import {usersReducer} from "./usersReducer";
-
 
 
 let rootReducer = combineReducers({
