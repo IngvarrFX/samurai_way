995fc337d9d3448fba259e88e3679d2ba3762a9a
IngvarrFX <64922169+IngvarrFX@users.noreply.github.com>
2021-11-18 12:22:44 +0300

    refactor test App component and create ProfileStatus.test.ts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import App from "./App";
+import {Provider} from "react-redux";
+import {store} from "./redux/reduxStore";
+import {BrowserRouter} from "react-router-dom";
+import ReactDOM from "react-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>, div);
+    ReactDOM.unmountComponentAtNode(div)
 });
